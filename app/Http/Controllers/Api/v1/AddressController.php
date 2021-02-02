17b41b11d195d7b57d2a87a4c972e5cdf4bfbdef
Tyler O'Brien <hello@tylerobrien.com>
2021-02-02 19:09:02 +0000

    Fix wrong namespace being used

--- app/Http/Controllers/Api/v1/AddressController.php
+++ app/Http/Controllers/Api/v1/AddressController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\v1\Admin;
+namespace App\Http\Controllers\Api\v1;
 
 use App\Models\Address;
 use App\Traits\Controllers\Api\v1\HasQueryFilter;
--- app/Http/Controllers/Api/v1/ImageController.php
+++ app/Http/Controllers/Api/v1/ImageController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\v1\Admin;
+namespace App\Http\Controllers\Api\v1;
 
 use App\Models\Image;
 use App\Traits\Controllers\Api\v1\HasQueryFilter;
