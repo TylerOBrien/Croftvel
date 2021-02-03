a086bfc965f1fbb2c542261aeaf3a41395011acc
Tyler O'Brien <hello@tylerobrien.com>
2021-02-03 16:48:59 +0000

    Add create files migration

---
+++ database/migrations/2020_06_08_195409_create_files_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('disk', [ 'local', 'public', 's3' ]);
+            $table->string('name');
+            $table->string('filepath');
+            $table->string('mimetype');
+            $table->unsignedMediumInteger('filesize');
+            $table->unsignedBigInteger('owner_id');
+            $table->string('owner_type');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->datetime('created_at')->useCurrent();
+            $table->datetime('updated_at')->nullable();
+
+            $table->index('owner_id');
+            $table->index('owner_type');
+
+            $table->foreign('created_by')
+                  ->references('id')->on('users')
+                  ->onDelete('set null')
+                  ->onUpdate('restrict');
+            
+            $table->foreign('updated_by')
+                  ->references('id')->on('users')
+                  ->onDelete('set null')
+                  ->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
