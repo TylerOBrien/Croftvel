eb61441964d601648ed0603e1d586ef889e17197
Tyler O'Brien <hello@tylerobrien.com>
2021-02-03 04:46:52 +0000

    Fix validators

--- app/Providers/ValidationServiceProvider.php
+++ app/Providers/ValidationServiceProvider.php
@@ -2,8 +2,7 @@
 
 namespace App\Providers;
 
-use App\Rules\Country;
-use App\Rules\Province;
+use App\Rules\{ Country, Province, MatchesCurrent };
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
@@ -27,12 +26,16 @@ class ValidationServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Validator::extend('country', function($attribute, $value, $parameter, $validator) {
+        Validator::extendImplicit('country', function($attribute, $value, $parameter, $validator) {
             return (new Country)->passes($attribute, $value);
         });
 
-        Validator::extend('province', function($attribute, $value, $parameter, $validator) {
+        Validator::extendImplicit('province', function($attribute, $value, $parameter, $validator) {
             return (new Province)->passes($attribute, $value);
+        });
+
+        Validator::extendImplicit('matches_current', function($attribute, $value, $parameter, $validator) {
+            return (new MatchesCurrent)->passes($attribute, $value);
         });
     }
 }
