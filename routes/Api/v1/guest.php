d07325cdd0dfa17be6d3620ba53667a221fae90b
Tyler O'Brien <hello@tylerobrien.com>
2021-02-10 01:10:38 +0000

    Update routes

--- routes/Api/v1/guest.php
+++ routes/Api/v1/guest.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\v1\{ TokenController, UserController };
+namespace App\Http\Controllers\Api\v1;
 
 use Illuminate\Support\Facades\Route;
 
--- routes/Api/v1/user.php
+++ routes/Api/v1/user.php
@@ -1,20 +1,27 @@
 <?php
 
-use App\Http\Controllers\Api\v1\{ IdentityController, TokenController, UserController, VerificationController };
+namespace App\Http\Controllers\Api\v1;
 
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('accounts', TokenController::class);
-Route::apiResource('addresses', TokenController::class);
-Route::apiResource('files', TokenController::class);
+Route::apiResource('accounts', AccountController::class);
+Route::apiResource('addresses', AddressController::class);
+Route::apiResource('files', FileController::class);
 
 Route::put('identities/{identity}/verification', [ IdentityController::class, 'verify' ]);
 Route::apiResource('identities', IdentityController::class);
 
-Route::apiResource('images', TokenController::class);
-Route::apiResource('metas', TokenController::class);
-Route::apiResource('secrets', TokenController::class);
+Route::apiResource('images', ImageController::class);
+Route::apiResource('metas', MetaController::class);
+
+Route::apiResource('profiles/{profile}/floats', ProfileController::class);
+Route::apiResource('profiles/{profile}/integers', ProfileController::class);
+Route::apiResource('profiles/{profile}/strings', ProfileController::class);
+Route::apiResource('profiles/{profile}/texts', ProfileController::class);
+Route::apiResource('profiles', ProfileController::class);
+
+Route::apiResource('secrets', SecretController::class);
 Route::apiResource('tokens', TokenController::class)->except('store');
 Route::apiResource('users', UserController::class)->except('store');
 Route::apiResource('verifications', VerificationController::class);
-Route::apiResource('videos', UserController::class);
+Route::apiResource('videos', VideoController::class);
