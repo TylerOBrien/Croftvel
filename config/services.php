dbe3d95b10e09d8de8106b9c41528d357fe2526f
Tyler O'Brien <hello@tylerobrien.com>
2021-02-10 17:58:49 +0000

    Add GitHub config

--- config/services.php
+++ config/services.php
@@ -30,4 +30,12 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'github' => [
+        'client' => [
+            'id' => env('GITHUB_CLIENT_ID'),
+            'secret' => env('GITHUB_CLIENT_SECRET')
+        ],
+        'redirect' => env('GITHUB_REDIRECT_URL'),
+    ]
+
 ];
