02440626b971470d165d0b388a6b60514496b252
Tyler O'Brien <hello@tylerobrien.com>
2021-02-10 05:34:44 +0000

    Clean up code

--- app/Traits/Models/HasUniqueMaker.php
+++ app/Traits/Models/HasUniqueMaker.php
@@ -14,13 +14,13 @@ trait HasUniqueMaker
      */
     static public function makeUniqueInt(string $column, int $digits=12):int
     {
-        $where = self::class . '::where';
+        $model = self::class;
         $min = intval(str_repeat('1', $digits));
         $max = intval(str_repeat('9', $digits));
 
         do {
             $id = random_int($min, $max);
-        } while (call_user_func($where, [ $column => $id ])->count());
+        } while ($model::where([ $column => $id ])->count());
 
         return $id;
     }
@@ -33,11 +33,11 @@ trait HasUniqueMaker
      */
     static public function makeUniqueString(string $column, int $length=12):string
     {
-        $where = self::class . '::where';
+        $model = self::class;
 
         do {
             $id = Str::random($length);
-        } while (call_user_func($where, [ $column => $id ])->count());
+        } while ($model::where([ $column => $id ])->count());
 
         return $id;
     }
