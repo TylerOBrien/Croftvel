ee829f70f4459018a5a9a6e4c44bc6f2c5ebec5e
Tyler O'Brien <hello@tylerobrien.com>
2021-02-10 00:23:26 +0000

    Update create users migration

--- database/migrations/2021_02_08_000003_create_users_table.php
+++ database/migrations/2021_02_08_000003_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('middle_name')->nullable();
             $table->string('last_name')->nullable();
             $table->boolean('is_active')->default(1);
+            $table->datetime('last_active_at')->nullable();
             $table->datetime('created_at')->useCurrent();
             $table->datetime('updated_at')->nullable();
 
