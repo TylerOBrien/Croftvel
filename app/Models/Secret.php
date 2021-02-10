3be6504b63e27483d7279b3a227f5754910f9c15
Tyler O'Brien <hello@tylerobrien.com>
2021-02-10 00:57:12 +0000

    Update Models

--- app/Models/Secret.php
+++ app/Models/Secret.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Secret extends Model
 {
-    //
+    protected $hidden = [
+        'value'
+    ];
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'value'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
