7744de5a31555819c0f4cc69c1e0d2941f285d53
Tyler O'Brien <hello@tylerobrien.com>
2021-02-11 19:27:10 +0000

    Rename event to match Laravel convention

--- app/Events/Api/v1/Recovery/RecoveryCreated.php
+++ app/Events/Api/v1/Recovery/RecoveryCreated.php
@@ -8,7 +8,7 @@ use Illuminate\Broadcasting\{ InteractsWithSockets, PrivateChannel };
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class CreatedRecovery
+class RecoveryCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
