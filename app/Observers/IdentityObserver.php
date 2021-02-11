b182251ab1695a5a4a6d7103bbf05cac24cc3c57
Tyler O'Brien <hello@tylerobrien.com>
2021-02-11 22:09:48 +0000

    Use observer for Identity

---
+++ app/Observers/IdentityObserver.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Observers;
+
+use App\Events\Api\v1\Identity\IdentityVerified;
+use App\Models\Identity;
+
+class IdentityObserver
+{
+    /**
+     * Handle the Identity "updated" event.
+     *
+     * @param  \App\Models\Identity  $identity
+     * @return void
+     */
+    public function updated(Identity $identity)
+    {
+        if ($identity->wasChanged('verified_at')) {
+            event(new IdentityVerified($identity));
+        } 
+    }
+}
