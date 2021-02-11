20b679f9cb67d73b355827697456f7e06bb11c56
Tyler O'Brien <hello@tylerobrien.com>
2021-02-11 20:35:26 +0000

    Listen for UserIdentified event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,9 +4,11 @@ namespace App\Providers;
 
 use App\Events\Api\v1\Identity\IdentityCreated;
 use App\Events\Api\v1\Recovery\RecoveryCreated;
+use App\Events\Api\v1\User\UserIdentified;
 
 use App\Listeners\Api\v1\Identity\SendVerifyIdentityNotification;
 use App\Listeners\Api\v1\Recovery\SendVerifyRecoveryNotification;
+use App\Listeners\Api\v1\User\SendWelcomeUserNotification;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as BaseEventServiceProvider;
 
@@ -24,6 +26,10 @@ class EventServiceProvider extends BaseEventServiceProvider
         
         RecoveryCreated::class => [
             SendVerifyRecoveryNotification::class
+        ],
+        
+        UserIdentified::class => [
+            SendWelcomeUserNotification::class
         ]
     ];
 
