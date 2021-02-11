b0851c4f02792511338c08197383a9c9bfe4664d
Tyler O'Brien <hello@tylerobrien.com>
2021-02-11 19:07:11 +0000

    Add VerifyRecovery notification

---
+++ app/Mail/Api/v1/Recovery/VerifyRecovery.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Mail\Api\v1\Recovery;
+
+use App\Models\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class VerifyRecovery extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var \App\Models\User $user
+     */
+    protected $user;
+
+    /**
+     * The plaintext recovery code.
+     * 
+     * @var string
+     */
+    protected $code;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, string $code)
+    {
+        $this->user = $user;
+        $this->code = $code;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject(trans('mail.guest.recovery.verify-recovery.subject'))
+                    ->markdown('mail.guest.recovery.verify-recovery', [
+                        'user' => $this->user,
+                        'code' => $this->code ]);
+    }
+}
---
+++ app/Notifications/Api/v1/VerifyRecoveryNotification.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Notifications\Api\v1;
+
+use App\Mail\Api\v1\Recovery\VerifyRecovery;
+use App\Models\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class VerifyRecoveryNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * The plaintext recovery code.
+     * 
+     * @var string
+     */
+    protected $code;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(string $code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  \App\Models\User  $notifiable
+     * 
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail(User $notifiable)
+    {
+        $mail = new VerifyRecovery($notifiable, $this->code);
+        $mail->to($notifiable->email, $notifiable->full_name);
+        $mail->from(config('mail.from.address'), config('mail.from.name'));
+
+        return $mail;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
