6a52f45e8d11f3bde80ee8d6643dd813f4b5d8ab
Tyler O'Brien <hello@tylerobrien.com>
2021-02-11 02:50:51 +0000

    Add privileges

---
+++ database/migrations/2021_02_08_000005_create_abilities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abilities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('privilege_id');
+            $table->string('name');
+            $table->string('model');
+            $table->datetime('created_at')->useCurrent();
+            $table->datetime('updated_at')->nullable();
+
+            $table->foreign('privilege_id')
+                  ->references('id')->on('privileges')
+                  ->onDelete('cascade')
+                  ->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abilities');
+    }
+}
