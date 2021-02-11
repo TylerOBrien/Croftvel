602a80578f814b588df9bf6a8601c7577e544603
Tyler O'Brien <hello@tylerobrien.com>
2021-02-11 00:47:31 +0000

    Update Province rule

--- app/Rules/Province.php
+++ app/Rules/Province.php
@@ -2,21 +2,49 @@
 
 namespace App\Rules;
 
+use L91\ISO_3166_2\Subdivision;
+
 use Illuminate\Contracts\Validation\Rule;
 
 class Province implements Rule
 {
     /**
+     * The country code this province is expected to belong to.
+     *
+     * @var string
+     */
+    protected $country_code;
+    
+    /**
+     * Create a new rule.
+     *
+     * @param  string  $country_code
+     * 
+     * @return bool
+     */
+    public function __construct(string $country_code)
+    {
+        $this->country_code = strtoupper($country_code);
+    }
+
+    /**
      * Determine if the validation rule passes.
      *
      * @param  string  $attribute
-     * @param  mixed   $value
+     * @param  mixed  $value
      * 
      * @return bool
      */
     public function passes($attribute, $value)
     {
-        return true;
+        $value = strtoupper($value);
+        $data = Subdivision::getSubdivisions($this->country_code);
+
+        if (!$subdivisions = get_object_vars($data)) {
+            return false;
+        }
+
+        return in_array("{$this->country_code}-{$value}", array_keys($subdivisions));
     }
 
     /**
