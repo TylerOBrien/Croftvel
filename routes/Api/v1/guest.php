c3c486c728a71b6e6ca8d02e614fd016290aec19
Tyler O'Brien <hello@tylerobrien.com>
2021-02-11 18:38:52 +0000

    Add recoveries routes

--- routes/Api/v1/guest.php
+++ routes/Api/v1/guest.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\v1;
 
 use Illuminate\Support\Facades\Route;
 
+Route::apiResource('recoveries', RecoveryController::class);
+
 Route::post('accounts', [ AccountController::class, 'store' ]);
 Route::post('tokens',  [ TokenController::class, 'store' ]);
 Route::post('users', [ UserController::class, 'store' ]);
