6cb6cbd7e8345d1ef5b11ebc77110892a605f777
Tyler O'Brien <hello@tylerobrien.com>
2021-02-12 19:44:49 +0000

    Clean up code

--- app/Http/Controllers/Api/v1/MetaIntegerController.php
+++ app/Http/Controllers/Api/v1/MetaIntegerController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-use App\Models\{ Meta, MetaInteger };
-use App\Traits\Controllers\Api\v1\HasControllerHelpers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\v1\MetaInteger\{ IndexMetaInteger, ShowMetaInteger, StoreMetaInteger, UpdateMetaInteger, DestroyMetaInteger };
+use App\Models\{ Meta, MetaInteger };
+use App\Traits\Controllers\Api\v1\HasControllerHelpers;
 
 class MetaIntegerController extends Controller
 {
