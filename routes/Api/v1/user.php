f109bf26bc6624a533fe4921dbf4de1e6477d2ec
Tyler O'Brien <hello@tylerobrien.com>
2021-02-13 02:30:44 +0000

    Add secrets routes to user

--- routes/Api/v1/user.php
+++ routes/Api/v1/user.php
@@ -10,9 +10,11 @@ Route::apiResource('files', FileController::class);
 Route::apiResource('images', ImageController::class);
 Route::apiResource('secrets', SecretController::class);
 Route::apiResource('tokens', TokenController::class)->except('store');
-Route::apiResource('users', UserController::class)->except('store');
 Route::apiResource('verifications', VerificationController::class);
 Route::apiResource('videos', VideoController::class);
+
+Route::apiResource('users', UserController::class)->except('store');
+Route::apiResource('users/{user}/secrets', SecretController::class);
 
 Route::put('identities/{identity}/verification', [ IdentityController::class, 'verify' ]);
 Route::apiResource('identities', IdentityController::class);
