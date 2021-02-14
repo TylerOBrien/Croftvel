7e6d09233082eff8a313fdac61bf8bffc24b6454
Tyler O'Brien <hello@tylerobrien.com>
2021-02-14 22:57:44 +0000

    Update mail

--- resources/views/mail/identity/verify-identity.blade.php
+++ resources/views/mail/identity/verify-identity.blade.php
@@ -9,7 +9,7 @@
 
 Please verify you are the owner of this email address.
 
-Your secret code is {{ $plaintext_code }}.
+Your secret verification code is {{ chunk_split($plaintext_code, 3, '—') }}.
 
 Thanks,<br>
 {{ config('app.name') }}
