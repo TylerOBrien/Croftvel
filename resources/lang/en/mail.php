5e497de922787676ee9b1dfb28a1762802de04ca
Tyler O'Brien <hello@tylerobrien.com>
2021-02-15 20:42:57 +0000

    Update mail lang

--- resources/lang/en/mail.php
+++ resources/lang/en/mail.php
@@ -3,11 +3,20 @@
 return [
 
     'user' => [
-        'password-reset-request' => [
-            'subject' => 'Password Reset Request'
+        'added-to-account' => [
+            'subject' => 'A user was added to your account'
         ],
+
+        'removed-from-account' => [
+            'subject' => 'A user was removed from your account'
+        ],
+
+        'password-reset-request' => [
+            'subject' => 'Password reset request'
+        ],
+
         'verify-email' => [
-            'subject' => 'Verify Your Email'
+            'subject' => 'Verify your email address'
         ]
     ]
 
