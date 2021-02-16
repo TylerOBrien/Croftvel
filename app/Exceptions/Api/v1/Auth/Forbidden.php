503ffe2b4b34ce5a09e1cb8029a141d419ad0271
Tyler O'Brien <hello@tylerobrien.com>
2021-02-16 19:04:50 +0000

    Pass forbidden ability info

--- app/Exceptions/Api/v1/Auth/Forbidden.php
+++ app/Exceptions/Api/v1/Auth/Forbidden.php
@@ -4,15 +4,29 @@ namespace App\Exceptions\Api\v1\Auth;
 
 use App\Exceptions\ApiException;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
 class Forbidden extends ApiException
 {
     /**
      * Create a new exception.
      * 
+     * @param  string  $ability
+     * @param  string|\Illuminate\Database\Eloquent\Model  $target
+     * 
      * @return void
      */
-    public function __construct()
+    public function __construct(string $ability, $target)
     {
-        parent::__construct('auth.forbidden', 403);
+        if (is_string($target)) {
+            $target = '.' . strtolower(Str::afterLast($target, '\\'));
+        } else if ($target instanceof Model) {
+            $target = '.' . Str::singular($target->getTable()) . '.' . $target->id;
+        } else {
+            // error
+        }
+
+        parent::__construct('auth.forbidden', 403, compact('ability', 'target'));
     }
 }
