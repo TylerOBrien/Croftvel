2ffd29132bdad78f65c6ce30496ac368e8727eb9
Tyler O'Brien <hello@tylerobrien.com>
2021-02-16 01:17:26 +0000

    Restrict guests to Recovery.Store only

--- routes/Api/v1/guest.php
+++ routes/Api/v1/guest.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers\Api\v1;
 
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('recoveries', RecoveryController::class);
-
+Route::post('recoveries', [ RecoveryController::class, 'store' ]);
 Route::post('tokens',  [ TokenController::class, 'store' ]);
 Route::post('users', [ UserController::class, 'store' ]);
 
