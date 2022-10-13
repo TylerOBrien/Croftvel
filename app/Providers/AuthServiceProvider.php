fbd48036fad12ed112e05e76a45b2c2cd3fcabde
Tyler O'Brien <hello@tylerobrien.com>
2022-10-12 20:35:04 -0400

    Associate models with policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,30 @@ namespace App\Providers;
 
 use App\Guards\Api\v1\ApiGuard;
 
+use App\Models\Ability;
+use App\Models\Account;
+use App\Models\Address;
+use App\Models\File;
+use App\Models\Identity;
+use App\Models\Image;
+use App\Models\Profile;
+use App\Models\Secret;
+use App\Models\User;
+use App\Models\Verification;
+use App\Models\Video;
+
+use App\Policies\Api\v1\AbilityPolicy;
+use App\Policies\Api\v1\AccountPolicy;
+use App\Policies\Api\v1\AddressPolicy;
+use App\Policies\Api\v1\FilePolicy;
+use App\Policies\Api\v1\IdentityPolicy;
+use App\Policies\Api\v1\ImagePolicy;
+use App\Policies\Api\v1\ProfilePolicy;
+use App\Policies\Api\v1\SecretPolicy;
+use App\Policies\Api\v1\UserPolicy;
+use App\Policies\Api\v1\VerificationPolicy;
+use App\Policies\Api\v1\VideoPolicy;
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as BaseAuthServiceProvider;
 use Illuminate\Support\Facades\{ Auth, Gate };
 
@@ -15,7 +39,20 @@ class AuthServiceProvider extends BaseAuthServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        Ability::class => AbilityPolicy::class,
+        Account::class => AccountPolicy::class,
+        Address::class => AddressPolicy::class,
+        File::class => FilePolicy::class,
+        Identity::class => IdentityPolicy::class,
+        Image::class => ImagePolicy::class,
+        Meta::class => MetaPolicy::class,
+        Privilege::class => PrivilegePolicy::class,
+        Profile::class => ProfilePolicy::class,
+        PersonalAccessToken::class => PersonalAccessTokenPolicy::class,
+        Secret::class => SecretPolicy::class,
+        User::class => UserPolicy::class,
+        Verification::class => VerificationPolicy::class,
+        Video::class => VideoPolicy::class,
     ];
 
     /**
