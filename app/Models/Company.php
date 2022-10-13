3674811b66a25935c006434e6c0921cd0c0a5f01
Tyler O'Brien <hello@tylerobrien.com>
2022-10-12 21:37:38 -0400

    Add traits to Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Traits\Models\{ HasAddresses, HasProfiles };
+
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    use HasAddresses, HasProfiles;
 }
