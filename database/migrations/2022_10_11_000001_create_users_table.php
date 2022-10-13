c1c26f56c44582d73bdabb592345c6e453db4b44
Tyler O'Brien <hello@tylerobrien.com>
2022-10-12 21:11:53 -0400

    Add name columns to users table

--- database/migrations/2022_10_11_000001_create_users_table.php
+++ database/migrations/2022_10_11_000001_create_users_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('account_id')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->boolean('is_enabled')->default(1)->index();
             $table->timestamp('last_active_at')->nullable();
             $table->timestamp('identified_at')->nullable();
