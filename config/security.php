4c4c6b3273a916ae72919d65a2be4b63a961647e
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 12:50:41 -0400

    Add new security config file

---
+++ config/security.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Token
+    |--------------------------------------------------------------------------
+    |
+    | Settings related to user authentication tokens.
+    |
+    */
+
+    'token' => [
+        'name' => env('APP_TOKEN_NAME', env('APP_NAME', 'Laravel')),
+        'ttl' => env('SECURITY_TOKEN_TTL', 10080), /* 7 days */
+        'length' => env('SECURITY_TOKEN_LENGTH', 40),
+        'hash_algo' => 'sha256',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Recovery
+    |--------------------------------------------------------------------------
+    |
+    | Settings related to account recoveries (i.e. forgot password attempts).
+    |
+    */
+
+    'recovery' => [
+        'ttl' => env('SECURITY_RECOVERY_TTL', 60),
+        'length' => env('SECURITY_RECOVERY_LENGTH', 12),
+        'hash_algo' => 'sha256',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Verification
+    |--------------------------------------------------------------------------
+    |
+    | Settings related to verification attempts, which will typically only be
+    | email and/or mobile number verification for newly created accounts.
+    |
+    */
+
+    'verification' => [
+        'ttl' => env('SECURITY_VERIFICATION_TTL', 60),
+        'length' => env('SECURITY_VERIFICATION_LENGTH', 12),
+        'hash_algo' => 'sha256',
+    ],
+
+];
