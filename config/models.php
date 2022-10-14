67bed914cfad38fa39eb429f8e54e0a36cf36519
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 12:59:55 -0400

    Add new models config file

---
+++ config/models.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Namespace
+    |--------------------------------------------------------------------------
+    |
+    | The prefix for the fully qualified namespace of all models. It is very
+    | unlikely that this will need to be used/modified, but it is provided here
+    | as a convenience for all utilities to use to future-proof against any
+    | changes to the project structure.
+    |
+    */
+
+    'namespace' => 'App\Models\\',
+
+];
