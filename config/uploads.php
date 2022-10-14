59b9221d2d11be6ad918be8bd5e3077a11951d3f
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 13:49:56 -0400

    Add max filesize settings for uploads

--- config/uploads.php
+++ config/uploads.php
@@ -14,21 +14,25 @@ return [
     'default' => [
         'disk' => 'public',
         'dest' => 'upload',
+        'max-filesize' => '32M',
     ],
 
     'files' => [
         'disk' => 'public',
         'dest' => 'files',
+        'max-filesize' => '32M',
     ],
 
     'images' => [
         'disk' => 'public',
         'dest' => 'images',
+        'max-filesize' => '32M',
     ],
 
     'videos' => [
         'disk' => 'public',
         'dest' => 'videos',
+        'max-filesize' => '32M',
     ],
 
 ];
