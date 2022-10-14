1f006ed785f68cf82415ae5273c91fd1a9142b2b
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 16:37:09 -0400

    Rename attributes var to fields and add comments

--- app/Models/File.php
+++ app/Models/File.php
@@ -44,30 +44,30 @@ class File extends Model
     /**
      * Create a new File model from an uploaded file.
      *
-     * @param  \Illuminate\Http\UploadedFile  $file
-     * @param  array  $attributes
+     * @param  \Illuminate\Http\UploadedFile  $file  The file uploaded by a user.
+     * @param  array  $fields  The attributes passed to the create function, typically coming from a request.
      *
      * @return \App\Models\File
      */
-    static public function createFromFile(UploadedFile $file, array $attributes) : File
+    static public function createFromFile(UploadedFile $file, array $fields) : File
     {
-        return self::createFromFileBase($file, $attributes, config('uploads.file.dest'));
+        return self::createFromFileBase($file, $fields, config('uploads.file.dest'));
     }
 
     /**
      * Modify an existing File model using an uploaded file.
      *
-     * @param  \Illuminate\Http\UploadedFile  $file
-     * @param  array  $attributes
+     * @param  \Illuminate\Http\UploadedFile  $file  The file uploaded by a user.
+     * @param  array  $fields  The attributes passed to the update function, typically coming from a request.
      *
      * @return bool
      */
-    public function updateFromFile(UploadedFile $file, array $attributes) : bool
+    public function updateFromFile(UploadedFile $file, array $fields) : bool
     {
         return call_user_func(
             [ $this, 'updateFromFileBase' ],
             $file,
-            $attributes,
+            $fields,
             config('uploads.file.dest'),
         );
     }
