e48631a655e35440e768e946e6f258e4e88cdb1e
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 14:52:18 -0400

    Add Ability::disallow function

--- app/Models/Ability.php
+++ app/Models/Ability.php
@@ -48,4 +48,33 @@ class Ability extends Model
             ]);
         }
     }
+
+    /**
+     * Remove any existing permissions previously granted to the user for the
+     * defined ability/abilities. This will disallow the user from performing
+     * actions on models.
+     *
+     * @param  \App\Models\User  $user  The user to remove permission from.
+     * @param  \Illuminate\Database\Eloquent\Model  $resource  The resource the ability had been applied to.
+     * @param  array|string  $abilities  The ability to remove from the user.
+     *
+     * @return void
+     */
+    static public function disallow(User $user, $resource, array|string $abilities) : void
+    {
+        $privilege = $user->privilege;
+
+        if (is_string($abilities)) {
+            $abilities = [ $abilities ];
+        }
+
+        foreach ($abilities as $ability) {
+            self::where([
+                'name' => $ability,
+                'privilege_id' => $privilege->id,
+                'model_id' => is_string($resource) ? null : $resource->id,
+                'model_type' => is_string($resource) ? $resource : $resource::class,
+            ])->delete();
+        }
+    }
 }
