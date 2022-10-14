fd38158e60643ae5228a4ed8368b7a5c99b06e93
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 12:32:38 -0400

    Clean up code and add comments

--- config/enum.php
+++ config/enum.php
@@ -1,13 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Enums
-|--------------------------------------------------------------------------
-|
-*/
-
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enums
+    |--------------------------------------------------------------------------
+    |
+    | The valid values for all enum columns in the database. These are used
+    | during migrations, so the side-effects of modifying existing values must
+    | be considered otherwise there will be a risk of truncated data in the
+    | database.
+    |
+    */
+
     'identity' => [
         'type' => [
             'email',
@@ -47,4 +53,5 @@ return [
             'token',
         ],
     ],
+
 ];
