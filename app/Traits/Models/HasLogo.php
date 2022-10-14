0f3a6d036eba051d6172e7cf7be05ac5e80a5e0d
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 16:02:07 -0400

    Add typehint

--- app/Traits/Models/HasLogo.php
+++ app/Traits/Models/HasLogo.php
@@ -2,14 +2,16 @@
 
 namespace App\Traits\Models;
 
+use App\Models\Image;
+
 trait HasLogo
 {
     use HasImages;
 
     /**
-     * @return \App\Models\Image
+     * @return \App\Models\Image|null
      */
-    public function getLogoAttribute()
+    public function getLogoAttribute() : Image|null
     {
         return $this->images()->whereName('logo.primary.xl')->first();
     }
