cdeb4f261661889dd7c461fbd9cc8b279a4b615e
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 15:56:59 -0400

    Use addressUpdateRules as source of address rules

--- app/Http/Requests/Api/v1/Address/UpdateAddress.php
+++ app/Http/Requests/Api/v1/Address/UpdateAddress.php
@@ -4,11 +4,11 @@ namespace App\Http\Requests\Api\v1\Address;
 
 use App\Http\Requests\Api\v1\ApiRequest;
 use App\Models\Address;
-use App\Traits\Requests\Api\v1\HasOwnership;
+use App\Traits\Requests\Api\v1\{ HasAddress, HasOwnership };
 
 class UpdateAddress extends ApiRequest
 {
-    use HasOwnership;
+    use HasAddress, HasOwnership;
 
     /**
      * Instantiate the request.
@@ -29,16 +29,12 @@ class UpdateAddress extends ApiRequest
      */
     public function rules()
     {
-        return [
-            'owner_id' => 'required_with:owner_type|morphable',
-            'owner_type' => 'required_with:owner_id|morphable',
-            'name' => 'string',
-            'line1' => 'nullable|string',
-            'line2' => 'nullable|string',
-            'city' => 'nullable|string',
-            'subdivision' => 'required_with:country|subdivision:' . request('country'),
-            'country' => 'required_with:subdivision|country',
-            'postal_code' => 'nullable|string'
-        ];
+        return array_merge($this->addressUpdateRules(),
+            [
+                'name' => 'required|string',
+                'owner_id' => 'required_with:owner_type|morphable',
+                'owner_type' => 'required_with:owner_id|morphable',
+            ],
+        );
     }
 }
