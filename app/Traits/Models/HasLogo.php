64dc41ca8f75b1b9abe4f30e389e2da83945bb08
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 17:54:08 -0400

    Add createLogo func to HasLogo

--- app/Traits/Models/HasLogo.php
+++ app/Traits/Models/HasLogo.php
@@ -2,11 +2,31 @@
 
 namespace App\Traits\Models;
 
+use App\Models\Image;
+
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Collection;
 
 trait HasLogo
 {
     use HasImages;
+
+    /**
+     * Creates a new Image instance preconfigured to be able to represent a logo.
+     *
+     * @param  \Illuminate\Http\UploadedFile  $file  The image file uploaded by a user.
+     * @param  string  $name  The name of the logo.
+     *
+     * @return \App\Models\Image
+     */
+    public function createLogo(UploadedFile $file, string $name = 'primary') : Image
+    {
+        return Image::createFromFile($file, [
+            'name' => "logo.$name.xl",
+            'owner_id' => $this->id,
+            'owner_type' => $this::class,
+        ]);
+    }
 
     /**
      * @return \Illuminate\Support\Collection|null
