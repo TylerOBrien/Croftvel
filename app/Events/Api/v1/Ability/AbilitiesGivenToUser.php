4cf8b74dad6a6ba71bd9ace6def4a07fa84abfad
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 15:15:17 -0400

    Fix ability events not properly matching permissions schema

--- app/Events/Api/v1/Ability/AbilitiesGivenToUser.php
+++ app/Events/Api/v1/Ability/AbilitiesGivenToUser.php
@@ -5,7 +5,6 @@ namespace App\Events\Api\v1\Account;
 use App\Models\User;
 
 use Illuminate\Broadcasting\{ InteractsWithSockets, PrivateChannel };
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -14,30 +13,44 @@ class AbilitiesGivenToUser
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     /**
-     * The User model instance that was given abilities.
+     * The user model instance that was given abilities.
      *
      * @var \App\Models\User
      */
     public $user;
 
     /**
-     * The Ability model instances that were given to the user.
+     * The resource the abilities were applied to.
      *
-     * @var \Illuminate\Support\Database\Collection
+     * Is either the string version of the model's fully qualified class name,
+     * which would mean the abilities apply to all instances of the model, or
+     * is a singular instance of the model, which would mean the ability will
+     * only apply to that instance.
+     *
+     * @var string|\Illuminate\Database\Eloquent\Model
+     */
+    public $resource;
+
+    /**
+     * The names of the abilities given to the user.
+     *
+     * @var array<string>
      */
     public $abilities;
 
     /**
      * Create a new event instance.
      *
-     * @param \App\Models\User  $user  The user that was given abilities.
-     * @param \Illuminate\Database\Eloquent\Collection  $abilities  The Ability model instances that were given to the user.
+     * @param  \App\Models\User  $user  The user that was given abilities.
+     * @param  string|\Illuminate\Database\Eloquent\Model  $resource  The resource the abilities applied to.
+     * @param  array<string>  $abilities  The names of the abilities given to the user.
      *
      * @return void
      */
-    public function __construct(User $user, Collection $abilities)
+    public function __construct(User $user, $resource, array $abilities)
     {
         $this->user = $user;
+        $this->resource = $resource;
         $this->abilities = $abilities;
     }
 
