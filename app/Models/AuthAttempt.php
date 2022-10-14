95cf8932c0c1b7499f74b8d770eb2fbee42d4582
Tyler O'Brien <hello@tylerobrien.com>
2022-10-14 14:22:03 -0400

    Add fromIdentity to AuthAttempt model

--- app/Models/AuthAttempt.php
+++ app/Models/AuthAttempt.php
@@ -21,4 +21,17 @@ class AuthAttempt extends Model
     protected $dispatchesEvents = [
         'created' => AuthAttemptCreated::class,
     ];
+
+    /**
+     * Builds a query constraining all AuthAttempt rows to the passed Identity.
+     *
+     * @param  \App\Models\Identity  $identity  The identity instance to lookup auth attempts for.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    static public function fromIdentity(Identity $identity) : \Illuminate\Database\Eloquent\Builder
+    {
+        return self::where('identity_type', $identity->type)
+                   ->where('identity_value', $identity->value);
+    }
 }
