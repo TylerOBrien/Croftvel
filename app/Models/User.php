7fce1bc013fe296660697e9f23b3d5cca29e1ddf
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 18:03:37 -0400

    Add full name attributes to appends

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,8 @@ class User extends BaseUser
     use Notifiable, HasFactory, HasEnabledState, HasFullName, HasUserAbilities, HasProfiles;
 
     protected $appends = [
+        'full_name',
+        'full_name_reverse',
         'is_identified',
     ];
 
