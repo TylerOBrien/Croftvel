1cd6ba1f6d0df44ff4b524379a0c01535d00dd8e
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 01:37:17 -0400

    Cast is_enabled to bool

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -14,6 +14,7 @@ class Account extends Model
 
     protected $casts = [
         'status' => AccountStatus::class,
+        'is_enabled' => 'boolean',
     ];
 
     protected $dispatchesEvents = [
