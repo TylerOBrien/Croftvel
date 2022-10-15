c095e464a58f018128cb63dc7b0ca822ce7d9d6c
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 13:50:44 -0400

    Add AccountIsEnabled middleware

---
+++ app/Http/Middleware/Api/v1/AccountIsEnabled.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware\Api\v1;
+
+use Closure;
+
+use App\Exceptions\Api\v1\Account\AccountDisabled;
+use App\Guards\Api\v1\ApiGuard;
+
+use Illuminate\Http\Request;
+
+class AccountIsEnabled
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $account = ApiGuard::get()->user()?->account;
+
+        if (!$account?->is_enabled) {
+            throw new AccountDisabled;
+        }
+
+        return $next($request);
+    }
+}
