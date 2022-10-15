a6fe89a69b1fa9fcced9141a6366010c943a361f
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 14:35:39 -0400

    Rename function

--- app/Traits/Models/HasOwnership.php
+++ app/Traits/Models/HasOwnership.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 trait HasOwnership
@@ -26,5 +27,27 @@ trait HasOwnership
     public function setOwnerTypeAttribute(string $owner_type)
     {
         $this->attributes['owner_type'] = Str::start($owner_type, config('models.namespace'));
+    }
+
+    /**
+     * Attempts to retrieve an instance of this model that has the given name
+     * and is owned by the given owner resource. It is assumed that the name
+     * is unique. If there are multiple rows with the same name and owner then
+     * only the first row found will be returned.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $owner  The resource that owns the model instance to be found.
+     * @param  string  $name  The name of the model instance to be found.
+     *
+     * @return self|null
+     */
+    static public function findFromOwner(Model $owner, string|null $name = null): self|null
+    {
+        $predicate = [
+            'name' => $name ?? config('models.default.name'),
+            'owner_id' => $owner->id,
+            'owner_type' => $owner::class,
+        ];
+
+        return self::where($predicate)->first();
     }
 }
