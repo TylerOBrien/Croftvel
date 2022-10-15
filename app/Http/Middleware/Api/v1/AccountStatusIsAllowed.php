a26de02f2ae02401d510ab8ed1efa2dc7395f673
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 13:31:53 -0400

    Add AccountStatusIsAllowed middleware

---
+++ app/Http/Middleware/Api/v1/AccountStatusIsAllowed.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware\Api\v1;
+
+use Closure;
+
+use App\Enums\Account\AccountStatus;
+use App\Exceptions\Api\v1\Account\AccountSuspended;
+use App\Guards\Api\v1\ApiGuard;
+
+use Illuminate\Http\Request;
+
+class AccountStatusIsAllowed
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = ApiGuard::get()->user();
+
+        switch ($user->account ?? null) {
+        case AccountStatus::Suspended:
+            throw new AccountSuspended;
+        }
+
+        return $next($request);
+    }
+}
