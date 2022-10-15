d408588b6086c49f38c3f01dd163f0edbfe59d06
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 01:12:10 -0400

    Clean up code

--- app/Rules/PhoneNumber.php
+++ app/Rules/PhoneNumber.php
@@ -25,7 +25,8 @@ class PhoneNumber implements Rule
      */
     public function passes($attribute, $value)
     {
-        return strlen($value) >= 10 && preg_match('%^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$%i', $value);
+        return strlen($value) >= 10 &&
+               preg_match('%^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$%i', $value);
     }
 
     /**
