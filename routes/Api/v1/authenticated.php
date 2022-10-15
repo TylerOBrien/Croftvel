f0d12e56018c2f84ddd1a3b5fe0934e673af2b50
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 17:56:44 -0400

    Add /identities endpoint

--- routes/Api/v1/authenticated.php
+++ routes/Api/v1/authenticated.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('addresses', AddressController::class);
 Route::apiResource('files', FileController::class);
+Route::apiResource('identities', IdentityController::class);
 Route::apiResource('images', ImageController::class);
 Route::apiResource('users', UserController::class);
 Route::apiResource('videos', VideoController::class);
