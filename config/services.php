191cf1cbd2d04b5a53b54cd7af7c60de5bb83b54
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 19:33:39 -0400

    Add github settings to services config

--- config/services.php
+++ config/services.php
@@ -31,4 +31,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'github' => [
+        'type' => 'oauth',
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('APP_URL_API') . '/v1/oauth/providers/github/user',
+    ],
+
 ];
