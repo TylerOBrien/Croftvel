b4c67ea2397fb8830c74c6271a1aac825ea08851
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 15:31:04 -0400

    Update comments and typehints

--- app/Traits/Models/HasVerify.php
+++ app/Traits/Models/HasVerify.php
@@ -15,8 +15,12 @@ use App\Exceptions\Api\v1\Verify\{
 trait HasVerify
 {
     /**
-     * @param  string  $ability
-     * @param  array  $fields
+     * Attempts to verify this model against the given ability name and
+     * verification code field data. This will throw an error if any check
+     * fails.
+     *
+     * @param  string  $ability  The ability (e.g. store or recover) to verify.
+     * @param  array  $fields  The field data contaning the verification code to check against.
      *
      * @return bool
      */
@@ -55,12 +59,14 @@ trait HasVerify
     }
 
     /**
-     * @param  \App\Models\Verification  $verification
-     * @param  string  $plaintext_code
+     * Attempts to verify the verification instance against the plaintext code.
+     *
+     * @param  \App\Models\Verification  $verification  The verification instance from storage.
+     * @param  string  $plaintext_code  The verification code in plaintext.
      *
      * @return void
      */
-    protected function verifyByCode(Verification $verification, string $plaintext_code)
+    protected function verifyByCode(Verification $verification, string $plaintext_code): void
     {
         $plaintext_code = str_replace(['-', '.', ' '], '', $plaintext_code);
         $hashed_code = hash(config('security.verification.hash_algo'), $plaintext_code);
@@ -71,12 +77,12 @@ trait HasVerify
     }
 
     /**
-     * @param  \App\Models\Verification  $verification
+     * @param  \App\Models\Verification  $verification  The verification instance from storage.
      * @param  string  $token
      *
      * @return void
      */
-    protected function verifyByToken(Verification $verification, string $token)
+    protected function verifyByToken(Verification $verification, string $token): void
     {
         if (($this->oauth_token->value ?? null) !== $token) {
             throw new InvalidVerificationCode;
