ef86d16975cdd85f66c05beac93839189f6f283e
Tyler O'Brien <hello@tylerobrien.com>
2022-10-16 04:04:27 -0400

    Add TwitterUser resource

---
+++ app/Models/TwitterUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TwitterUser extends Model
+{
+    protected $table = 'twitter_users';
+
+    protected $fillable = [
+        'identity_id',
+        'twitter_id',
+        'nickname',
+        'email',
+        'profile_image_url',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function identity()
+    {
+        return $this->belongsTo(Identity::class);
+    }
+}
---
+++ database/migrations/2022_10_16_011416_create_twitter_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('twitter_users', function (Blueprint $table) {
+            $table->unsignedBigInteger('twitter_id')->primary();
+            $table->unsignedBigInteger('identity_id');
+            $table->string('nickname');
+            $table->string('email')->nullable();
+            $table->string('profile_image_url')->nullable();
+            $table->timestamps();
+
+            $table->foreign('identity_id')
+                  ->references('id')->on('identities')
+                  ->onDelete('cascade')
+                  ->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('twitter_users');
+    }
+};
