fdbf767706375d7324593f72341f12ffccf4fb71
Tyler O'Brien <hello@tylerobrien.com>
2022-10-16 19:29:56 -0400

    Add LinkedIn enum

--- app/Enums/OAuth/OAuthProvider.php
+++ app/Enums/OAuth/OAuthProvider.php
@@ -7,5 +7,6 @@ enum OAuthProvider: string
     case Facebook = 'facebook';
     case GitHub = 'github';
     case Google = 'google';
+    case LinkedIn = 'linkedin';
     case Twitter = 'twitter';
 }
