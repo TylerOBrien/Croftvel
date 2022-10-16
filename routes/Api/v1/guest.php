787c5484e583bfd35692750651b020ed1475f792
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 20:26:09 -0400

    Add oauth endpoints

--- routes/Api/v1/guest.php
+++ routes/Api/v1/guest.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api\v1;
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('oauth/providers/{provider}', [ OAuthController::class, 'provider' ]);
+Route::get('oauth/providers/{provider}/user', [ OAuthController::class, 'user' ]);
+
 Route::post('identities/verifications', VerifyIdentityController::class);
 Route::post('recoveries', ForgotPasswordController::class);
 Route::post('recoveries/verifications', VerifyRecoveryController::class);
