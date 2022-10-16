6aaddd4fa89486e62aab326504a44013bed26c17
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 20:58:33 -0400

    Add better error handling support

--- app/Http/Requests/Api/v1/OAuthRequest.php
+++ app/Http/Requests/Api/v1/OAuthRequest.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Requests\Api\v1;
 
-use App\Exceptions\Api\v1\OAuth\ProviderNotFound;
+use App\Exceptions\Api\v1\OAuth\{ InvalidToken, ProviderNotFound };
 use App\Http\Requests\Api\v1\ApiRequest;
+use App\Support\OAuth\OAuthDriver;
+
+use GuzzleHttp\Exception\ClientException;
 
 class OAuthRequest extends ApiRequest
 {
@@ -14,11 +17,23 @@ class OAuthRequest extends ApiRequest
      */
     public function __construct()
     {
-        $given = request()->route('provider');
-        $providers = config('enum.oauth.provider', []);
+        $provider = request()->route('provider');
 
-        if (!in_array($given, $providers)) {
+        if (!in_array($provider, config('enum.oauth.provider'))) {
             throw new ProviderNotFound;
+        }
+
+        if (request()->has('code')) {
+            try {
+                OAuthDriver::get($provider)->stateless()->user();
+            } catch (ClientException $exception) {
+                switch ($exception->getResponse()->getStatusCode()) {
+                case 401:
+                    throw new InvalidToken;
+                default:
+                    throw $exception;
+                }
+            }
         }
     }
 }
