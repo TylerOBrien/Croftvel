a597038464753e3fc99a7b30dcd15f8fd2290b74
Tyler O'Brien <hello@tylerobrien.com>
2022-10-16 15:08:59 -0400

    Add OAuthProvider support class

---
+++ app/Support/OAuth/OAuthProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Support\OAuth;
+
+use App\Enums\OAuth\OAuthProvider as OAuthProviderEnum;
+use Laravel\Socialite\Two\{
+    FacebookProvider,
+    GithubProvider,
+    GoogleProvider,
+    TwitterProvider,
+};
+
+class OAuthProvider
+{
+    /**
+     * Attempts to retrieve the string-based name of the given OAuth provider.
+     * Will return null if the appropriate name cannot be found.
+     *
+     * @param  \Laravel\Socialite\Two\FacebookProvider|GithubProvider|GoogleProvider|TwitterProvider  $driver  The driver to get the name of.
+     *
+     * @return string|null
+     */
+    static public function name(FacebookProvider|GithubProvider|GoogleProvider|TwitterProvider $driver): string|null
+    {
+        return self::enum($driver)?->value;
+    }
+
+    /**
+     * Attempts to retrieve the PHP enum for the given OAuth provider.
+     * Will return null if the appropriate name cannot be found.
+     *
+     * @param  \Laravel\Socialite\Two\FacebookProvider|GithubProvider|GoogleProvider|TwitterProvider  $driver  The driver to get the name of.
+     *
+     * @return OAuthProviderEnum|null
+     */
+    static public function enum(FacebookProvider|GithubProvider|GoogleProvider|TwitterProvider $driver): OAuthProviderEnum|null
+    {
+        if ($driver instanceof FacebookProvider) {
+            return OAuthProviderEnum::Facebook;
+        }
+
+        if ($driver instanceof GithubProvider) {
+            return OAuthProviderEnum::GitHub;
+        }
+
+        if ($driver instanceof GoogleProvider) {
+            return OAuthProviderEnum::Google;
+        }
+
+        if ($driver instanceof TwitterProvider) {
+            return OAuthProviderEnum::Twitter;
+        }
+
+        return null;
+    }
+}
