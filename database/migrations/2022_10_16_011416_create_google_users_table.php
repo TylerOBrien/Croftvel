5f8b865a80c508b5da2b8bb200bc8b8f821c71cb
Tyler O'Brien <hello@tylerobrien.com>
2022-10-16 13:44:59 -0400

    Add create google_users table migration

---
+++ database/migrations/2022_10_16_011416_create_google_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('google_users', function (Blueprint $table) {
+            $table->unsignedBigInteger('google_id')->primary();
+            $table->unsignedBigInteger('identity_id');
+            $table->string('nickname')->nullable();
+            $table->string('email')->nullable();
+            $table->string('profile_image_url')->nullable();
+            $table->timestamps();
+
+            $table->foreign('identity_id')
+                  ->references('id')->on('identities')
+                  ->onDelete('cascade')
+                  ->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('google_users');
+    }
+};
