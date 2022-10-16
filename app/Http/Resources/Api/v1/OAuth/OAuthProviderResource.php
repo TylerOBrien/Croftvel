c76ea10f5651fc5d48b5d7804a9331befbe54fd8
Tyler O'Brien <hello@tylerobrien.com>
2022-10-16 15:15:51 -0400

    Store enum of which provider is used

--- app/Http/Resources/Api/v1/OAuth/OAuthProviderResource.php
+++ app/Http/Resources/Api/v1/OAuth/OAuthProviderResource.php
@@ -2,10 +2,35 @@
 
 namespace App\Http\Resources\Api\v1\OAuth;
 
+use App\Support\OAuth\OAuthProvider;
+
 use Illuminate\Http\Resources\Json\JsonResource;
+
+use Laravel\Socialite\Two\{ FacebookProvider, GithubProvider, GoogleProvider, TwitterProvider };
 
 class OAuthProviderResource extends JsonResource
 {
+    /**
+     * The OAuth provider.
+     *
+     * @var \App\Enums\OAuth\OAuthProvider
+     */
+    protected $provider;
+
+    /**
+     * Instantiate the resource.
+     *
+     * @param  \Laravel\Socialite\Two\FacebookProvider|GithubProvider|GoogleProvider|TwitterProvider  $driver
+     *
+     * @return array
+     */
+    public function __construct(FacebookProvider|GithubProvider|GoogleProvider|TwitterProvider $driver)
+    {
+        $this->provider = OAuthProvider::enum($driver);
+
+        parent::__construct($driver);
+    }
+
     /**
      * Transform the resource into an array.
      *
