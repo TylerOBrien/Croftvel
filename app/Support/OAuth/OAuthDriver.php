0c2c2dc58fac988cf7264c52b2765192978f632f
Tyler O'Brien <hello@tylerobrien.com>
2022-10-15 20:19:47 -0400

    Add OAuthDriver helper

---
+++ app/Support/OAuth/OAuthDriver.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Support\OAuth;
+
+use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\{ FacebookProvider, GithubProvider, GoogleProvider };
+
+class OAuthDriver
+{
+    /**
+     * This is a helper function that exists to prevent false positive linter
+     * errors when using the provider returned by Socialite::driver.
+     *
+     * @param  string  $provider  The name of the OAuth provider.
+     *
+     * @return FacebookProvider|GithubProvider|GoogleProvider
+     */
+    static public function get(string $provider): FacebookProvider|GithubProvider|GoogleProvider
+    {
+        return Socialite::driver($provider);
+    }
+}
