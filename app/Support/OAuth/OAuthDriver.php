8f4905a4965984f95be6fe0103acd66b15099f8d
Tyler O'Brien <hello@tylerobrien.com>
2022-10-16 03:07:47 -0400

    Add token and user helper funcs

--- app/Support/OAuth/OAuthDriver.php
+++ app/Support/OAuth/OAuthDriver.php
@@ -2,11 +2,33 @@
 
 namespace App\Support\OAuth;
 
+use App\Models\Identity;
+
 use Laravel\Socialite\Facades\Socialite;
-use Laravel\Socialite\Two\{ FacebookProvider, GithubProvider, GoogleProvider };
+use Laravel\Socialite\Two\{ User as SocialiteUser, FacebookProvider, GithubProvider, GoogleProvider };
 
 class OAuthDriver
 {
+    /**
+     * @param  \App\Models\Identity  $identity
+     *
+     * @return string
+     */
+    static public function token(Identity $identity): string
+    {
+        return self::user($identity)->token;
+    }
+
+    /**
+     * @param  \App\Models\Identity  $identity
+     *
+     * @return \Laravel\Socialite\Two\User
+     */
+    static public function user(Identity $identity): SocialiteUser
+    {
+        return OAuthDriver::get($identity->provider)->stateless()->user();
+    }
+
     /**
      * This is a helper function that exists to prevent false positive linter
      * errors when using the provider returned by Socialite::driver.
