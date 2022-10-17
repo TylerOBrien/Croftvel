ab1c99ee3f0e8938f8d78247e3a16f856aebbf4a
Tyler O'Brien <hello@tylerobrien.com>
2022-10-17 01:21:11 -0400

    Add /companies endpoint

--- routes/Api/v1/authenticated.php
+++ routes/Api/v1/authenticated.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v1;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('addresses', AddressController::class);
+Route::apiResource('companies', CompanyController::class);
 Route::apiResource('files', FileController::class);
 Route::apiResource('identities', IdentityController::class);
 Route::apiResource('images', ImageController::class);
