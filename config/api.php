f15d8b10f737dbc7493ec11766b634a5d917794c
Tyler O'Brien <hello@tylerobrien.com>
2022-10-17 01:03:30 -0400

    Add api.bearer settings

--- config/api.php
+++ config/api.php
@@ -38,4 +38,21 @@ return [
         'name' => env('API_GUARD_NAME', 'croft'),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | API Bearer
+    |--------------------------------------------------------------------------
+    |
+    | Settings for the Bearer token provided in the HTTP header.
+    |
+    */
+
+    'bearer' => [
+        'name' => env('API_BEARER_NAME', env('APP_NAME', 'Croftvel')),
+        'ttl' => env('API_BEARER_TTL', 10080),
+        'ttl_type' => env('API_BEARER_TTL_TYPE', 'minute'), // 10080 minutes = 7 days
+        'length' => env('API_BEARER_LENGTH', 40),
+        'hash_algo' => env('API_BEARER_HASH_ALGO', 'sha256'),
+    ]
+
 ];
