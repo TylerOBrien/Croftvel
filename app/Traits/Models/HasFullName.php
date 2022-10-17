068e157f6946bacefcbdc19732b30e1c473ac0d4
Tyler O'Brien <hello@tylerobrien.com>
2022-10-17 02:07:03 -0400

    Use if statements rather than func calls

--- app/Traits/Models/HasFullName.php
+++ app/Traits/Models/HasFullName.php
@@ -15,26 +15,30 @@ trait HasFullName
      */
     public function getFullNameAttribute(): string|null
     {
-        $full_name = implode(' ', array_filter([ $this->first_name, $this->last_name ]));
-
-        if (empty($full_name)) {
-            return null;
+        if ($this->first_name) {
+            if ($this->last_name) {
+                return $this->first_name.' '.$this->last_name;
+            } else {
+                return $this->first_name;
+            }
         }
 
-        return $full_name;
+        return $this->last_name;
     }
 
     /**
      * @return string|null
      */
-    public function getReversedFullName(): string|null
+    public function getReversedFullNameAttribute(): string|null
     {
-        $full_name = implode(', ', array_filter([ $this->last_name, $this->first_name ]));
-
-        if (empty($full_name)) {
-            return null;
+        if ($this->last_name) {
+            if ($this->first_name) {
+                return $this->last_name.', '.$this->first_name;
+            } else {
+                return $this->last_name;
+            }
         }
 
-        return $full_name;
+        return $this->first_name;
     }
 }
