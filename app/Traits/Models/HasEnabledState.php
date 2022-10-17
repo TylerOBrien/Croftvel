6de1b88d35e288e6e80715d9ee8f05b0fa24b0f5
Tyler O'Brien <hello@tylerobrien.com>
2022-10-17 01:48:08 -0400

    Remove call to save

--- app/Traits/Models/HasEnabledState.php
+++ app/Traits/Models/HasEnabledState.php
@@ -10,7 +10,6 @@ trait HasEnabledState
     public function enable()
     {
         $this->is_enabled = 1;
-        $this->save();
 
         return $this;
     }
@@ -21,7 +20,6 @@ trait HasEnabledState
     public function disable()
     {
         $this->is_enabled = 0;
-        $this->save();
 
         return $this;
     }
