09ce45d95a4cf381698e284650f7763fccb18db5
Tyler O'Brien <hello@tylerobrien.com>
2022-10-16 23:08:50 -0400

    Add arg so http_code can be optionally changed

--- app/Exceptions/Api/v1/Identity/IdentityIsntOAuth.php
+++ app/Exceptions/Api/v1/Identity/IdentityIsntOAuth.php
@@ -9,10 +9,12 @@ class IdentityIsntOAuth extends ApiException
     /**
      * Create a new exception.
      *
+     * @param  int  $http_code  The HTTP status code that will be used in the response.
+     *
      * @return void
      */
-    public function __construct()
+    public function __construct(int $http_code = 422)
     {
-        parent::__construct('identity.already-verified', 422);
+        parent::__construct('identity.already-verified', $http_code);
     }
 }
