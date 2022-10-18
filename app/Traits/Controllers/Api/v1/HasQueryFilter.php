2c9525ecb6faf1665e947644ab3b20063f0fc5c4
Tyler O'Brien <hello@tylerobrien.com>
2022-10-18 00:30:30 -0400

    Add HasMany to typehint

--- app/Traits/Controllers/Api/v1/HasQueryFilter.php
+++ app/Traits/Controllers/Api/v1/HasQueryFilter.php
@@ -5,18 +5,19 @@ namespace App\Traits\Controllers\Api\v1;
 use App\Support\Query;
 
 use Illuminate\Database\Eloquent\{ Builder, Collection };
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait HasQueryFilter
 {
     /**
      * Apply any constraints, if they exist, then execute the query.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder  $query  The query to apply filter constraints to.
+     * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Relations\HasMany  $query  The query to apply filter constraints to.
      * @param  array  $fields  The fields containing the raw constraint data.
      *
      * @return \Illuminate\Database\Eloquent\Collection
      */
-    protected function filtered(Builder $query, array $fields): Collection
+    protected function filtered(Builder|HasMany $query, array $fields): Collection
     {
         if (isset($fields['filter'])) {
             return Query::filter($query, $fields['filter'])->get();
