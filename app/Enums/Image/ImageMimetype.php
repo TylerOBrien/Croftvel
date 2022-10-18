4c9c40909ec80484700e432dae2fb903825697d5
Tyler O'Brien <hello@tylerobrien.com>
2022-10-18 16:27:39 -0400

    Restructure order of enum cases

--- app/Enums/Image/ImageMimetype.php
+++ app/Enums/Image/ImageMimetype.php
@@ -4,8 +4,8 @@ namespace App\Enums\Image;
 
 enum ImageMimetype: string
 {
-    case PNG = 'image/png';
-    case JPG = 'image/jpeg';
-    case GIF = 'image/gif';
     case BMP = 'image/bmp';
+    case GIF = 'image/gif';
+    case JPG = 'image/jpeg';
+    case PNG = 'image/png';
 }
