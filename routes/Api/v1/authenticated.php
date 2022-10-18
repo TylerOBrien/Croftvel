d2749293ea5dc9ea5132dc14ae6baf7267e4214d
Tyler O'Brien <hello@tylerobrien.com>
2022-10-18 00:31:31 -0400

    Add profile fields endpoints

--- routes/Api/v1/authenticated.php
+++ routes/Api/v1/authenticated.php
@@ -10,5 +10,7 @@ Route::apiResource('files', FileController::class);
 Route::apiResource('identities', IdentityController::class);
 Route::apiResource('images', ImageController::class);
 Route::apiResource('profiles', ProfileController::class);
+Route::get('profiles/{profile}/fields', [ ProfileFieldController::class, 'index' ]);
+Route::apiResource('profile-fields', ProfileFieldController::class);
 Route::apiResource('users', UserController::class);
 Route::apiResource('videos', VideoController::class);
